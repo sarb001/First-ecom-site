8e580d78ddfd20a8738b8bc069066864f1022ad8
SARB <mrsinghbusiness05@gmail.com>
2022-11-27 07:40:52 +0530

    Adding Dependencies

---
+++ ecom-vite/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='n-container'>
+        <div className="n-row">
+            <div className="n-col">
+                <span className='n-email'> mrsinghbusiness05@gmail.com </span>
+            </div>
+            <div className="n-col">
+             
+            </div>
+        </div>
+       Navbar
+    </div>
+  )
+}
+
+export default Navbar
