b5fa73a20495bd7c6f421cebec2053665c338624
SARB <mrsinghbusiness05@gmail.com>
2022-11-27 15:31:44 +0530

    Carousel Done

--- ecom-vite/src/components/Slider.jsx
+++ ecom-vite/src/components/Slider.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faArrowCircleLeft , faArrowCircleRight } from '@fortawesome/free-solid-svg-icons'
 
+import '../styles/Slider.css';
+
 const Slider  = () => {
     return(
         <div className="s-container">
-             <div className="s-arrow">
+             <div className="s-arrow left">
                  <FontAwesomeIcon icon = {faArrowCircleLeft} />
              </div>
-             <div className="s-arrow">
+             <div className="s-arrow right">
                  <FontAwesomeIcon icon = {faArrowCircleRight} />
              </div>
         </div>
