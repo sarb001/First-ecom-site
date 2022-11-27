3efdca7245024f8ea968564a8003a5e989b4cc80
SARB <mrsinghbusiness05@gmail.com>
2022-11-27 11:11:01 +0530

    Navbar Done

--- ecom-vite/src/components/Navbar.jsx
+++ ecom-vite/src/components/Navbar.jsx
@@ -1,19 +1,44 @@
 import React from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faShoppingBag ,faHeart , faArrowRightToBracket} from '@fortawesome/free-solid-svg-icons'
+
+import '../styles/Navbar.css';
 
 const Navbar = () => {
   return (
     <div className='n-container'>
         <div className="n-row">
             <div className="n-col">
-                <span className='n-email'>Email -- mrsinghbusiness05@gmail.com </span>
-            </div>
+                <span className='n-email'> mrsinghbusiness05@gmail.com </span>
+            </div> 
             <div className="n-col">
-                Login <FontAwesomeIcon icon="fa-duotone fa-square-right" />
+                <div className="socials">
+                    <a href = "/"> <span> Instagram   </span> </a>
+                    <a href = "/"> <span>  Fb </span> </a>
+                    <a href = "/"> <span>  Google </span> </a>
+                </div>
             </div>
+          
+        </div>
+
+        <div className="n-row">
+            <div className="n-col">
+                <a href = "/"> <img src = "/public/logi-img.png"   alt = "logoimg" /></a>
+            </div>
+            
+            <div className="n-col">
+                <div className="icons">
+                    <span>  <FontAwesomeIcon icon= {faArrowRightToBracket} /> Login  </span> 
+                    <span>  <FontAwesomeIcon icon= {faHeart} /> 0 </span>
+                    <span>  <FontAwesomeIcon icon= {faShoppingBag} /> 0 </span>
+                </div>
+             </div>
         </div>
     </div>
   )
 }
+
+
+
 
 export default Navbar
