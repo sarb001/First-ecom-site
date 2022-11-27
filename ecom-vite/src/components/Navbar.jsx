c8b45074e752023bfd3c33f707902570b76532f0
SARB <mrsinghbusiness05@gmail.com>
2022-11-27 08:48:16 +0530

    On the Navbar

--- ecom-vite/src/components/Navbar.jsx
+++ ecom-vite/src/components/Navbar.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 const Navbar = () => {
   return (
     <div className='n-container'>
         <div className="n-row">
             <div className="n-col">
-                <span className='n-email'> mrsinghbusiness05@gmail.com </span>
+                <span className='n-email'>Email -- mrsinghbusiness05@gmail.com </span>
             </div>
             <div className="n-col">
-             
+                Login <FontAwesomeIcon icon="fa-duotone fa-square-right" />
             </div>
         </div>
-       Navbar
     </div>
   )
 }
